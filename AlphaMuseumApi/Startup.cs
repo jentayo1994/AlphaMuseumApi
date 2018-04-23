f995a93e90b4501d0f876caa926070989eefea10
Joker <Joker@DESKTOP-TG7CFUB>
2018-04-24 00:08:54 +0300

    package installation

--- AlphaMuseumApi/Startup.cs
+++ AlphaMuseumApi/Startup.cs
@@ -15,6 +15,13 @@ namespace AlphaMuseumApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddJwtBearerClientAuthentication()
+                .AddConfigurationStore(o =>
+                {
+                    o.ConnectionString = "mongodb://localhost:27017";
+                    o.Database = "Auth";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +32,7 @@ namespace AlphaMuseumApi
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+
         }
     }
 }
